8e092eff59fb47969dc8fa5e81392b228ccd3f60
gordonshlai <gordonshlai@yahoo.com.hk>
2021-07-23 11:49:20 +0100

    Linking Between Pages

--- pages/news/[newsId].jsx
+++ pages/news/[newsId].jsx
@@ -3,12 +3,10 @@ import { useRouter } from "next/router";
 // our-domain.com/news/something-important
 
 function DetailPage() {
-  const router = useRouter()
+  const router = useRouter();
 
-  const newsId=router.query.newsId;
-  return(
-    <h1>The Detail Page</h1>
-  )
+  const newsId = router.query.newsId;
+  return <h1>The Detail Page</h1>;
 }
 
 export default DetailPage;
--- pages/news/index.jsx
+++ pages/news/index.jsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 // our-domain.com/news
 
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/first">The first link</Link>
+        </li>
+        <li>The second link</li>
+        <li>The third link</li>
+      </ul>
+    </>
+  );
 }
 
 export default NewsPage;
