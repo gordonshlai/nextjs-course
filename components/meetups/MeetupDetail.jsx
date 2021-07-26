bbd99ad6299ce44bbb627972f8c6508cc73a2d29
gordonshlai <gordonshlai@yahoo.com.hk>
2021-07-26 11:14:23 +0100

    More Static Site Generation (SSG) With getStaticProps

--- components/meetups/MeetupDetail.jsx
+++ components/meetups/MeetupDetail.jsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
 import classes from "./MeetupDetail.module.css";
 
 function MeetupDetail(props) {
   return (
     <section className={classes.detail}>
-      <img src={props.image} alt={props.image} />
+      <Image
+        src={props.image}
+        alt={props.image}
+        width={700}
+        height={400}
+        layout="intrinsic"
+        className={classes.image}
+      />
       <h1>{props.title}</h1>
       <address>{props.address}</address>
       <p>{props.description}</p>
