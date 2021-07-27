1efcd4dce95ab90681e7ee0f880c15ac14db7e62
gordonshlai <gordonshlai@yahoo.com.hk>
2021-07-27 10:43:55 +0100

    Dynamic Pages & getStaticProps & getStaticPaths

--- pages/[meetupId]/index.jsx
+++ pages/[meetupId]/index.jsx
@@ -12,8 +12,26 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
 export async function getStaticProps(context) {
-  //fetch data from a dingle meetup
+  //fetch data from a single meetup
 
   const meetupId = context.params.meetupId;
   console.log(meetupId);
